40707d7f99219b106901c578f6037655e5e5b0f1
achernosvitov <chernick71@gmail.com>
2024-05-04 00:02:53 +0400

    feat: add Vuetify ui-kit

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<v-app-bar
+		color="primary"
+		elevation="1"
+	>
+		<v-app-bar-title>
+			The Shop
+		</v-app-bar-title>
+
+		<template #append>
+			<v-btn
+				icon="mdi-cart"
+				rounded
+			/>
+		</template>
+	</v-app-bar>
+</template>
+
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,8 @@
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import { md3 } from 'vuetify/blueprints'
+
+export const vuetify = createVuetify({
+	blueprint: md3,
+})
